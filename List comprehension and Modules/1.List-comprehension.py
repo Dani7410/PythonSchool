ac1ed75e5bd7e883a566570f3747238299c7db1b
dacho-J <joketovicdaniel@gmail.com>
2021-02-23 13:54:03 +0100

    Just a commit

---
+++ List comprehension and Modules/1.List-comprehension.py
@@ -0,0 +1,18 @@
+#Create a list of capital letters in the english alphabet
+#--Code
+
+
+
+#---
+#Create a list of capital letter from the english aplhabet,
+#but exclude 4 with the Unicode code point of either 70, 75, 80, 85.
+#--Code
+
+
+#---
+#Create a list of capital letter from from the english aplhabet,
+#but exclude every second between F & O
+#--Code
+
+
+#---
